59c6acd1aa121ac8e472b83d748c679694c6a2eb
Jakub Pusiak <jakub.pusiak@gmail.com>
2019-08-26 01:10:09 +0200

    Responsive video.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -10,7 +10,8 @@ import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from '../Header/Header';
-import './general.scss';
+import '../../sass/general.scss';
+import '../../sass/embedVideo-container.scss';
 import './layout.scss';
 import Footer from '../Footer/Footer';
 
